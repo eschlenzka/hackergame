db2546d23d1360e2118abb326ae3e8d46cdf4a63
asqiir <asqiir@blaupost.de>
2018-11-24 23:18:19 +0100

    Checksum check dummy

---
+++ src/console_backend/Checksum.java
@@ -0,0 +1,9 @@
+package console_backend;
+
+public class Checksum {
+	private Checksum() {} //keine Objekte erstellen
+	
+	public static boolean check(int checksum) {
+		return checksum == 1; //TODO: just a dummy
+	}
+}
