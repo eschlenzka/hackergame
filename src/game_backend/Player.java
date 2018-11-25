7540c8ad264c3f6d197ae7b14cae022a3a23b67d
simon <simon@172.16.61.15>
2018-11-25 03:25:52 +0100

    Player update

--- src/game_backend/Player.java
+++ src/game_backend/Player.java
@@ -7,7 +7,6 @@ public class Player{
 	Player(int x_spawn,int y_spawn){
 	x_pos = x_spawn;
 	y_pos = y_spawn;
-	System.out.println("("+Integer.toString(x_pos)+","+Integer.toString(y_pos)+")");
 	}
 	public void updatePosition(int new_x,int new_y) {
 		x_pos = new_x;
