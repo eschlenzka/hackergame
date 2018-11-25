ab3e087c66b20276230aee432a133ea5527ac2ee
simon <simon@172.16.61.15>
2018-11-25 02:13:59 +0100

    small updates

--- src/game_backend/GameLogic.java
+++ src/game_backend/GameLogic.java
@@ -1,21 +1,29 @@
 package game_backend;
 
-//import java.awt.KeyEventDispatcher;
-//import java.awt.KeyboardFocusManager;
-//import java.awt.event.KeyEvent;
+import java.awt.KeyEventDispatcher;
+import java.awt.KeyboardFocusManager;
+import java.awt.event.*;
 import java.util.*;
-
+import frontend.*;
 public class GameLogic {
 	Player player;
 	List<Enemy> enemies = new ArrayList<Enemy>();
 	Board gameBoard;
+	KeyAdapter listener;
 	public static void main(String[] args) {
-		GameLogic test = new GameLogic();
+		Board gameBoard = new Board();
+		GameLogic test = new GameLogic(gameBoard);
 	}
-	GameLogic(Board board) {
+	public GameLogic(Board board) {
 		player = new Player(10, 10);
 		gameBoard = board;
-		mainloop();
+		Timer timer = new Timer();
+		timer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				mainloop();
+			}
+		}, 0, 1000);
 	}
 	public void spawnEnemy(int x_pos, int y_pos, EnemyTypes enemyType){
 		Enemy e = new Enemy(x_pos,y_pos,enemyType);
@@ -23,21 +31,20 @@ public class GameLogic {
 	}
 	
 	private void mainloop() {
-		while (true) {
-			int pl_x = player.getX();
-			int pl_y = player.getY();
-			// Test if enemy should be spawned (Proxy)
-			
-			// Get pressed keys (Graphics)
+	
+		int pl_x = player.getX();
+		int pl_y = player.getY();
+		
+		// Test if enemy should be spawned (Proxy)
+
+		// Get pressed keys (Graphics)
 //			String pressedKeys = keys.getPressedButtons();
 //			if (pressedKeys.contains('w'){}
-				
-			
-				
-			// Change position depending on pressed keys
-			// Be aware of boundaries!
-			// 
-			
+
+		// Change position depending on pressed keys
+		// Be aware of boundaries!
+		//
+
 //			if (pl_x >= 0)
 //				pl_x = 0;
 //			if (pl_x <= WIDTH)
@@ -48,6 +55,6 @@ public class GameLogic {
 //				pl_y = 0;
 			// Submit new positions (Graphics)
 			player.updatePosition(pl_x, pl_y);
-		}
+		
 	}
 }
